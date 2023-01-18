4d2effcd8200ecaed0702f84ff33b1508dbeccdc
InGyu Hwang <hwanginkyu42@naver.com>
2023-01-18 23:05:18 +0900

    #6 - fix: 기존 오류 코드 수정
    data.sql 을 추가하면서 생긴 오류를 해결

--- src/main/java/com/hwang/boardproject/repository/ArticleCommentRepository.java
+++ src/main/java/com/hwang/boardproject/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.hwang.boardproject.repository;
 
 import com.hwang.boardproject.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/com/hwang/boardproject/repository/ArticleRepository.java
+++ src/main/java/com/hwang/boardproject/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.hwang.boardproject.repository;
 
 import com.hwang.boardproject.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
