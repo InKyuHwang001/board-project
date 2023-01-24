0b16a9b042f8ac5cd6845c16af49a1479ef583aa
InGyu Hwang <hwanginkyu42@naver.com>
2023-01-24 22:26:31 +0900

    #20 - 게시글 서비스 로직의 테스트와 골격 잡기
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제

---
+++ src/main/java/com/hwang/boardproject/service/ArticleService.java
@@ -0,0 +1,38 @@
+package com.hwang.boardproject.service;
+
+import com.hwang.boardproject.domain.type.SearchType;
+import com.hwang.boardproject.dto.ArticleDto;
+import com.hwang.boardproject.dto.ArticleUpdateDto;
+import com.hwang.boardproject.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+}
